d135126a9bcb6786bb82b9c9f14de3430ceca4f6
Nicholas Ward <npward@berkeley.edu>
2021-04-22 16:57:39 -0700

    Merkle tree example skeleton

--- src/merkle_tree_example/mod.rs
+++ src/merkle_tree_example/mod.rs
@@ -1,12 +1,31 @@
-use ark_ff::Field;
-use ark_relations::r1cs::ConstraintSynthesizer;
+use ark_relations::r1cs::Field;
+use ark_relations::r1cs::{ConstraintSynthesizer, SynthesisError, ConstraintSystemRef};
+use ark_crypto_primitives::merkle_tree::{
+    Config, TwoToOneDigest, LeafDigest,
+    constraints::PathVar
+};
+use ark_crypto_primitives::crh::{CRHGadget, TwoToOneCRHGadget};
 
-pub struct MerkleTreeVerification<F: Field> {
-    
+pub struct MerkleTreeVerification<P, LeafH, TwoToOneH, F>
+where
+    P: Config,
+    LeafH: CRHGadget<P::LeafHash, F>,
+    TwoToOneH: TwoToOneCRHGadget<P::TwoToOneHash, F>,
+    F: Field,
+{
+    root: TwoToOneDigest<P>,
+    leaf: LeafDigest<P>,
+    authentication_path: PathVar<P, LeafH, TwoToOneH, F>,
 }
 
-impl<F: Field> ConstraintSynthesizer<F> for MerkleTreeVerification {
+impl<P, LeafH, TwoToOneH, F> ConstraintSynthesizer<F> for MerkleTreeVerification<P, LeafH, TwoToOneH, F>
+where
+    P: Config,
+    LeafH: CRHGadget<P::LeafHash, F>,
+    TwoToOneH: TwoToOneCRHGadget<P::TwoToOneHash, F>,
+    F: Field,
+{
     fn generate_constraints(self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
-        
+        Ok(())
     }
 }
