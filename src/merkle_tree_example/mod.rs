b73cdbc9650a24d480c0874dfae65a3e1878c429
Nicholas Ward <npward@berkeley.edu>
2021-04-23 10:02:35 -0700

    fixes and progress on MT example

--- src/merkle_tree_example/mod.rs
+++ src/merkle_tree_example/mod.rs
@@ -1,10 +1,12 @@
 use ark_relations::r1cs::Field;
 use ark_relations::r1cs::{ConstraintSynthesizer, SynthesisError, ConstraintSystemRef};
 use ark_crypto_primitives::merkle_tree::{
-    Config, TwoToOneDigest, LeafDigest,
+    Config, TwoToOneDigest, LeafDigest, Path,
     constraints::PathVar
 };
 use ark_crypto_primitives::crh::{CRHGadget, TwoToOneCRHGadget};
+use ark_std::marker::PhantomData;
+use ark_r1cs_std::alloc::{AllocVar, AllocationMode};
 
 pub struct MerkleTreeVerification<P, LeafH, TwoToOneH, F>
 where
@@ -15,7 +17,11 @@ where
 {
     root: TwoToOneDigest<P>,
     leaf: LeafDigest<P>,
-    authentication_path: PathVar<P, LeafH, TwoToOneH, F>,
+    authentication_path: Path<P>,
+
+    _leaf_h: PhantomData<LeafH>,
+    _two_to_one_h: PhantomData<TwoToOneH>,
+    _f: PhantomData<F>,
 }
 
 impl<P, LeafH, TwoToOneH, F> ConstraintSynthesizer<F> for MerkleTreeVerification<P, LeafH, TwoToOneH, F>
@@ -26,6 +32,12 @@ where
     F: Field,
 {
     fn generate_constraints(self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
+        let path_var : PathVar<P, LeafH, TwoToOneH, F> = PathVar::new_variable(
+            cs,
+            || Ok(self.authentication_path),
+            AllocationMode::Witness,
+        )?;
+
         Ok(())
     }
 }
