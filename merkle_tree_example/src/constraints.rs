17b573267b0b4a459daba0bc7523dbe4e1a98dce
ValarDragon <dojha12@gmail.com>
2021-04-26 09:31:56 -0700

    Add soundness test

--- merkle_tree_example/src/constraints.rs
+++ merkle_tree_example/src/constraints.rs
@@ -123,3 +123,66 @@ fn test_merkle_tree_constraints() {
     }
     assert!(is_satisfied);
 }
+
+// Run this test via `cargo test --release test_merkle_tree_constraints_soundness`.
+// This tests that a given invalid authentication path will fail.
+#[test]
+fn test_merkle_tree_constraints_soundness() {
+    use ark_relations::r1cs::{ConstraintSystem, ConstraintLayer, TracingMode};
+    use tracing_subscriber::layer::SubscriberExt;
+
+    // Let's set up an RNG for use within tests. Note that this is *not* safe
+    // for any production use.
+    let mut rng = ark_std::test_rng();
+
+    // First, let's sample the public parameters for the hash functions:
+    let leaf_crh_params = <LeafHash as CRH>::setup(&mut rng).unwrap();
+    let two_to_one_crh_params = <TwoToOneHash as TwoToOneCRH>::setup(&mut rng).unwrap();
+
+    // Next, let's construct our tree.
+    // This follows the API in https://github.com/arkworks-rs/crypto-primitives/blob/6be606259eab0aec010015e2cfd45e4f134cd9bf/src/merkle_tree/mod.rs#L156
+    let tree = crate::SimpleMerkleTree::new(
+        &leaf_crh_params,
+        &two_to_one_crh_params,
+        &[1u8, 2u8, 3u8, 10u8, 9u8, 17u8, 70u8, 45u8], // the i-th entry is the i-th leaf.
+    ).unwrap();
+
+    // We just mutate the first leaf
+    let second_tree = crate::SimpleMerkleTree::new(
+        &leaf_crh_params,
+        &two_to_one_crh_params,
+        &[4u8, 2u8, 3u8, 10u8, 9u8, 17u8, 70u8, 45u8], // the i-th entry is the i-th leaf.
+    ).unwrap();
+
+    // Now, let's try to generate a membership proof for the 5th item, i.e. 9.
+    let proof = tree.generate_proof(4).unwrap(); // we're 0-indexing!
+    
+    // But, let's get the root we want to verify against:
+    let wrong_root = second_tree.root();
+
+    let circuit = MerkleTreeVerification {
+        // constants
+        leaf_crh_params,
+        two_to_one_crh_params,
+
+        // public inputs
+        root: wrong_root,
+        leaf: 9u8,
+
+        // witness
+        authentication_path: Some(proof)
+    };
+    // First, some boilerplat that helps with debugging
+    let mut layer = ConstraintLayer::default();
+    layer.mode = TracingMode::OnlyConstraints;
+    let subscriber = tracing_subscriber::Registry::default().with(layer);
+    let _guard = tracing::subscriber::set_default(subscriber);
+
+    // Next, let's make the circuit!
+    let cs = ConstraintSystem::new_ref();
+    circuit.generate_constraints(cs.clone()).unwrap();
+    // Let's check whether the constraint system is satisfied
+    let is_satisfied = cs.is_satisfied().unwrap();
+    // We expec this to fail!
+    assert!(!is_satisfied);
+}
